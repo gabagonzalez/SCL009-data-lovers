43ea44303a77693eb73ad5efa54f3987b993cbed
Relun <claudia.araya.ro@gmail.com>
2019-04-24 11:56:39 -0400

    poniendo imagen y nombre en una tarjetita

--- src/main.js
+++ src/main.js
@@ -9,8 +9,18 @@ const container = document.getElementById("root");
 
 for(let i=0; i < lolValues.length; i++){  
 //const container = document.getElementById("root");
-container.innerHTML += `<p>${lolValues[i].id}</p>
-<img src="${lolValues[i].img}">`                                                           
+container.innerHTML += `
+
+<div class="card" >
+<div class="card-body float:left;">
+    <img src="${lolValues[i].img}" class="card-img-top" alt="...">
+    <div class="card-down float:right">
+    <h5 class="card-title">${lolValues[i].id}</h5>
+    </div>
+    </div>
+</div>
+
+`                                                           
     //console.log(lolValues[i].id);
 }
 /*
