21116a9e20eaabc311960a3127cd729edbdf6ab8
Relun <claudia.araya.ro@gmail.com>
2019-04-24 10:22:08 -0400

    creando código main.js para mostrar la data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
 /* Manejo del DOM */
+/* Manejo del DOM */
+
+const lolValues = Object.values(window.LOL.data);
+//const lolKeys = Object.keys(window.LOL.data);
+//console.log(lolValues);
+
+const container = document.getElementById("root");
+
+for(let i=0; i < lolValues.length; i++){  
+//const container = document.getElementById("root");
+container.innerHTML += `<p>${lolValues[i].id}</p>
+<img src="${lolValues[i].img}">`                                                           
+    //console.log(lolValues[i].id);
+}
+/*
+//vamos a meter todo al div "root"
+
+const showLol = (lolValues) => {
+ // quiero mostrar foto y nombre (id y img en lol.js)
+ //foreach: aquienvoyarecorrer.foreach(Element corresponde al Objeto Que esta Dentro De Cada Posicion) 
+ //foreachmuestra lo que esta adentrodesde el principio hasta el final
+ //element es como el data[i] muestra cada objeto
+
+ lolValues.forEach(element => {
+    
+     container.innerHTML += <p>${element.id}</p>
+     
+ });
+
+}*/
+
+window.onload = showLol(lolValues);
