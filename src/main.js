0e68d3aab0d597956a4b489007fba62d2c4b9e96
Relun <claudia.araya.ro@gmail.com>
2019-05-07 18:38:30 -0400

    modal insertado en la card con datos más especificos sobre los campeones, en main.js

--- src/main.js
+++ src/main.js
@@ -10,18 +10,47 @@ const sum = document.getElementById('calculation'); //div para mostrar calculo
 let showLol = (lolValues) => {
   for(let i=0; i < lolValues.length; i++){   
     container.innerHTML += `
-      <div id="flip-container">
-        <div class='cards'>
-         <li class='name'> ${lolValues[i].name}</li>
-          <li class='name'> ${lolValues[i].tags}</li>
-          <li class='name'> Dificult ${lolValues[i].info.difficulty}</li>
-          <img class='front' src="${lolValues[i].img}"/>
-          <hr>
-          <div class='back'>
-            <li class='title'> ${lolValues[i].difficulty}</li>
-          </div>
-        </div>
-      </div> 
+    <div id="flip-container">
+    <div class='cards'>
+     <li class='name'> ${lolValues[i].name}</li>
+      <li class='name'> ${lolValues[i].tags}</li>
+      <li class='name'> Dificult ${lolValues[i].info.difficulty}</li>
+      <img class='front' src="${lolValues[i].img}" data-toggle="modal" data-target="#exampleModal${lolValues[i].id}"/>
+      <hr>
+      <div class='back'>
+       <li class='title'> ${lolValues[i].difficulty}</li>
+      </div>
+    </div>
+  </div> <div class="modal fade" id="exampleModal${lolValues[i].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${lolValues[i].name}</h5>
+        
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+
+      </div>
+      <div class="modal-body"> 
+      <h5 class="modal-title">${lolValues[i].title}</h5>
+      <p>Ataque: ${lolValues[i].info.attack}</p>
+			<p>Defensa: ${lolValues[i].info.defense}</p>
+      <p>Magia: ${lolValues[i].info.magic}</p>
+      <p>Dificultad: ${lolValues[i].info.difficulty}</p>
+      <p>Vida: ${lolValues[i].stats.hp} (+${lolValues[i].stats.hpperlevel} por nivel)</p>
+      <p>Regeneración de Vida: ${lolValues[i].stats.hpregen} (+${lolValues[i].stats.hpregenperlevel} por nivel)</p>
+      <p>Daño de Ataque: ${lolValues[i].stats.attackdamage} (+${lolValues[i].stats.attackdamageperlevel} por nivel)</p>
+      <p>Velocidad de Movimiento: ${lolValues[i].stats.movespeed}</p>
+      <p>Armadura: ${lolValues[i].stats.armor} (+${lolValues[i].stats.armorperlevel} por nivel)</p>
+      <p>Resistencia Mágica: ${lolValues[i].stats.spellblock} (+${lolValues[i].stats.spellblockperlevel} por nivel)</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
     `  
   }
 }
@@ -59,17 +88,45 @@ document.getElementById('lol-sort').addEventListener('change', () => {
   container.innerHTML ="";
   newResult.forEach(element => {
     container.innerHTML += ` 
-      <div id="flip-container">
-        <div class='cards'>
-          <li class='name'> ${element.name}</li>
-          <li class='name'> Dificult ${element.info.difficulty}</li>
-          <img class='front' src="${element.img}"/>
-          <hr>
-          <div class='back'>
-            <li class='title'> ${element.info}</li>
-          </div>
-        </div>
-      </div> 
+    <div id="flip-container">
+    <div class='cards'>
+      <li class='name'> ${element.name}</li>
+      <li class='name'> Dificult ${element.info.difficulty}</li>
+      <img class='front' src="${element.img}" data-toggle="modal" data-target="#exampleModal${element.id}"/>
+      <hr>
+      <div class='back'>
+        <li class='title'> ${element.info}</li>
+      </div>
+    </div>
+  </div> 
+  <div class="modal fade" id="exampleModal${element.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${element.name}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>
+      <div class="modal-body">
+      <h5 class="modal-title">${element.title}</h5>
+      <p>Ataque: ${element.info.attack}</p>
+      <p>Defensa: ${element.info.defense}</p>
+      <p>Magia: ${element.info.magic}</p>
+      <p>Dificultad: ${element.info.difficulty}</p>
+      <p>Vida: ${element.stats.hp} (+${element.stats.hpperlevel} por nivel)</p>
+      <p>Regeneración de Vida: ${element.stats.hpregen} (+${element.stats.hpregenperlevel} por nivel)</p>
+      <p>Daño de Ataque: ${element.stats.attackdamage} (+${element.stats.attackdamageperlevel} por nivel)</p>
+      <p>Velocidad de Movimiento: ${element.stats.movespeed}</p>
+      <p>Armadura: ${element.stats.armor} (+${element.stats.armorperlevel} por nivel)</p>
+      <p>Resistencia Mágica: ${element.stats.spellblock} (+${element.stats.spellblockperlevel} por nivel)</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div> 
         ` 
   });
 });
@@ -87,13 +144,42 @@ document.getElementById("lol-filter").addEventListener("change",() => {
        <div class='cards'>
        <li class='name'> ${element.name}</li>
        <li class='name'> ${element.tags}</li>
-        <img class='front' src="${element.img}"/>
+       <img class='front' src="${element.img}" data-toggle="modal" data-target="#exampleModal${element.id}"/>
         <div class='back'>
           <li class='name'> ${element.name}</li>
         </div>
       </div>
       </div> 
       </div>
+      <div class="modal fade" id="exampleModal${element.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${element.name}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+
+      </div>
+      <div class="modal-body">
+      <h5 class="modal-title">${element.title}</h5>
+      <p>Ataque: ${element.info.attack}</p>
+			<p>Defensa: ${element.info.defense}</p>
+      <p>Magia: ${element.info.magic}</p>
+      <p>Dificultad: ${element.info.difficulty}</p>
+      <p>Vida: ${element.stats.hp} (+${element.stats.hpperlevel} por nivel)</p>
+      <p>Regeneración de Vida: ${element.stats.hpregen} (+${element.stats.hpregenperlevel} por nivel)</p>
+      <p>Daño de Ataque: ${element.stats.attackdamage} (+${element.stats.attackdamageperlevel} por nivel)</p>
+      <p>Velocidad de Movimiento: ${element.stats.movespeed}</p>
+      <p>Armadura: ${element.stats.armor} (+${element.stats.armorperlevel} por nivel)</p>
+      <p>Resistencia Mágica: ${element.stats.spellblock} (+${element.stats.spellblockperlevel} por nivel)</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
      `    
   });
 });
